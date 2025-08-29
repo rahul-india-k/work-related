f573f2fec49bd202aad7a38cbf8be8a43e83f14f
rahul-india-k <sankar.biswas.sb.market@gmail.com>
2025-08-29 23:11:41 +0530

    Update report-1.py

--- report-1.py
+++ report-1.py
@@ -1,43 +1,22 @@
-today = date.today()
-first_day = date(today.year, today.month, 1)
-last_day_prev_month = first_day - timedelta(days=1)
-process_date = last_day_prev_month.strftime("%Y-%m-%d")
+process_date = "2025-06-30"
 
-# 2. Union both sources in one go
-kxud_salesforce_data = (
-    spark.table("kes_clicks")
-        .filter(
-            (F.col("sourcename").isin("CKit KM Archive", "Salesforce KM")) &
-            (F.length("domain") > 0) &
-            (F.col("domain") != "Testing Admin Only") &
-            (~F.col("domain").contains(",")) &
-            (F.to_date("datetime") <= F.lit(process_date))
-        )
-        .withColumn(
-            "domain",
-            F.when(F.col("domain") == "SO Admin", "SO Operating Enablement")
-             .otherwise(F.col("domain"))
-        )
-        .withColumn("date", F.lit(process_date))
-        .select("date", "domain", F.countDistinct("clickid").alias("view_count"))
-        .groupBy("date", "domain").agg(F.sum("view_count").alias("view_count"))
-    .unionByName(
-        spark.table("kes_clicks_salesforce")
-            .filter(
-                (F.length("domain") > 0) &
-                (F.col("domain") != "Testing Admin Only") &
-                (~F.col("domain").contains(",")) &
-                (F.to_date("datetime") <= F.lit(process_date))
-            )
-            .withColumn(
-                "domain",
-                F.when(F.col("domain") == "SO Admin", "SO Operating Enablement")
-                 .otherwise(F.col("domain"))
-            )
-            .withColumn("date", F.lit(process_date))
-            .select("date", "domain", F.countDistinct("clickid").alias("view_count"))
-            .groupBy("date", "domain").agg(F.sum("view_count").alias("view_count"))
+bookmark_df = (
+    spark.table("EDW_BASE_O.KCS_BOOKMARK_VW").alias("A")
+    .join(
+        spark.table("EDW_BASE_O.KCS_KX_ARTICLE_VERSION_VW").alias("B"),
+        F.col("A.KNOWLEDGE_ARTICLE_C") == F.col("B.ARTICLE_VERSION_ID_C"),
+        "inner"
     )
+    .filter(
+        (F.to_date("A.CREATEDDATE") <= F.lit(process_date)) &
+        (F.col("B.ARTICLE_DOMAIN_C") != "Testing Admin Only")
+    )
+    .withColumn(
+        "domain",
+        F.when(F.col("B.ARTICLE_DOMAIN_C") == "SO Admin", "SO Operating Enablement")
+         .otherwise(F.col("B.ARTICLE_DOMAIN_C"))
+    )
+    .withColumn("date", F.lit(process_date))
     .groupBy("date", "domain")
-    .agg(F.sum("view_count").alias("view_count"))
+    .agg(F.countDistinct("A.ID").alias("bookmark_count"))
 )
